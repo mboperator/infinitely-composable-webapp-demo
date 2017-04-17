44476d64520d93afabf0ca868288096f1099ec59
Marcus Bernales <mboperator@gmail.com>
2017-04-16 17:38:38 -0700

    State tree is kinda broken

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import {getContext} from 'recompose';
+import {
+  Link,
+  Route,
+  BrowserRouter as Router,
+} from 'react-router-dom';
+
 import Dashboard from './components/Dashboard';
 import TreeChart from './components/TreeChart';
 
-const App = (props) => (
-  <div style={{ display: 'flex', flexDirection: 'column', width: '100%', height: '100%' }}>
-    <TreeChart
-      tree={props.dashboard}
-      size={500}
-      aspectRatio={0.5}
-      isSorted={false}
-      widthBetweenNodesCoeff={1.5}
-      heightBetweenNodesCoeff={2}
-      style={{border: '1px solid black'}}
-      tooltipOptions={{offset: {left: 30, top: 10}}}
-    />
-    <Dashboard />
-  </div>
+const ConfiguredTree = connect(s => {
+  return s.dashboard || {};
+})(
+  state => {
+    console.log('Tree props', state);
+    return (
+      <TreeChart
+        tree={state}
+        size={500}
+        aspectRatio={0.5}
+        isSorted={false}
+        widthBetweenNodesCoeff={1.5}
+        heightBetweenNodesCoeff={2}
+        style={{border: '1px solid black'}}
+        tooltipOptions={{offset: {left: 30, top: 10}}}
+      />
+    );
+  }
 );
 
-export default connect(s => s)(App);
+const App = (props) => (
+  <Router>
+    <div style={{ display: 'flex', flexDirection: 'column', width: '100%', height: '100%' }}>
+      <div>
+        <ul>
+          <li>
+            <Link to="/pokemonMe">
+              Pokemon Me
+            </Link>
+          </li>
+          <li>
+            <Link to="/stopwatch">
+              Stopwatch
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              Dashboard
+            </Link>
+          </li>
+        </ul>
+        <button>
+          State Visualizer
+        </button>
+      </div>
+      <ConfiguredTree />
+      <Dashboard />
+    </div>
+  </Router>
+);
+
+export default App;
